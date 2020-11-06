f7f932d5433744104d0323a34f7c6397f2386526
Carolina Salazar <65572735+carosalazar28@users.noreply.github.com>
2020-11-06 17:20:16 -0500

    add styles for component Form from Form.css (#1)
    * add styles for component Form from Form.css
    
    * add background color

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import App from '../App.css'
-
+import './styles/Form.css'
 class Form extends React.Component {
   state = {
     name: '',
@@ -12,7 +11,7 @@ class Form extends React.Component {
   }
 
   handleChange = (e) => {
-    const { name, lastName, email, password, terms, userType, type, checked, value } = e.target
+    const { name, type, checked, value } = e.target
     this.setState({ [name]: type === 'checkbox' ? checked : value })
   }
 
@@ -33,87 +32,115 @@ class Form extends React.Component {
     console.log(this.state)
     return(
       <form
-        className='App'
+        className='Form'
         onSubmit={this.handleSubmit}
       >
-        <label
-          htmlFor='name'
-        >
-          Nombre
-        </label>
-        <input
-          id='name'
-          type='text'
-          name='name'
-          value={this.name}
-          onChange={this.handleChange}
-        />
-        <label
-          htmlFor='lastName'
-        >
-          Apellido
-        </label>
-        <input
-          id='lastName'
-          type='text'
-          name='lastName'
-          value={this.lastName}
-          onChange={this.handleChange}
-        />
-        <label
-          htmlFor='email'
-        >
-          Correo Electrónico
-        </label>
-        <input
-          id='email'
-          type='email'
-          name='email'
-          value={this.email}
-          onChange={this.handleChange}
-        />
-        <label
-          htmlFor='password'
-        >
-          Contraseña
-        </label>
-        <input
-          id='password'
-          type='password'
-          name='password'
-          value={this.pasword}
-          onChange={this.handleChange}
-        />
-        <label
-          htmlFor='terms'
-        ><input
-          id='terms'
-          type='checkbox'
-          name='terms'
-          value={this.terms}
-          onChange={this.handleChange}
-        />
-          Acepto los Términos y Condiciones
-        </label>
-        <label>Tipo de Usuario:
-        </label>
-        <select
-          id='userType'
-          name='userType'
-          value={this.userType}
-          onChange={this.handleChange}
+        <span>
+          <label
+            className='Form__name-label'
+            htmlFor='name'
           >
-          <option value='cliente'>
-          Cliente
-          </option>
-          <option value='restaurante'>
-          Restaurante
-          </option>
-        </select>
-        <input
-          type='submit'
-          value='Enviar'
-        />
+            Nombre
+          </label>
+          <input
+            className='Form__name-input'
+            id='name'
+            type='text'
+            name='name'
+            value={this.name}
+            onChange={this.handleChange}
+          />
+        </span>
+        <span>
+          <label
+            className='Form__lastName-label'
+            htmlFor='lastName'
+          >
+            Apellido
+          </label>
+          <input
+            className='Form__lastName-input'
+            id='lastName'
+            type='text'
+            name='lastName'
+            value={this.lastName}
+            onChange={this.handleChange}
+          />
+        </span>
+        <span>
+          <label
+            className='Form__email-label'
+            htmlFor='email'
+          >
+            Correo Electrónico
+          </label>
+          <input
+            className='Form__email-input'
+            id='email'
+            type='email'
+            name='email'
+            value={this.email}
+            onChange={this.handleChange}
+          />
+        </span>
+        <span>
+          <label
+            className='Form__password-label'
+            htmlFor='password'
+          >
+            Contraseña
+          </label>
+          <input
+            className='Form__password-input'
+            id='password'
+            type='password'
+            name='password'
+            value={this.pasword}
+            onChange={this.handleChange}
+          />
+        </span>
+        <span>
+          <label
+            className='Form__terms-label'
+            htmlFor='terms'
+          >
+            Acepto los Términos y Condiciones
+          </label>
+          <input
+            className='Form__terms-input'
+            id='terms'
+            type='checkbox'
+            name='terms'
+            value={this.terms}
+            onChange={this.handleChange}
+          />
+        </span>
+        <span>
+          <label
+            className='Form__userType-label'
+          >Tipo de Usuario:
+          </label>
+          <select
+            id='userType'
+            name='userType'
+            value={this.userType}
+            onChange={this.handleChange}
+          >
+            <option value='cliente'>
+              Cliente
+            </option>
+            <option value='restaurante'>
+              Restaurante
+            </option>
+          </select>
+        </span>
+        <span className='Form__subtmit-span'>
+          <input
+            className='Form__submit-input'
+            type='submit'
+            value='Enviar'
+          />
+        </span>
       </form>
     )
   }
